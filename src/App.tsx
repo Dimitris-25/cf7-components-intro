befeeed43ddcb3dc9dd394b42a7927dbba8015e7
Dim. Baltas <fenomeno979@gmail.com>
2025-07-29 11:49:31 +0300

    feat: new components created

--- src/App.tsx
+++ src/App.tsx
@@ -1,33 +1,22 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import ViteIntro from "./pages/ViteIntro.tsx";
+
+import ClassComponent from "./component/ClassComponent.tsx";
+import FunctionalComponent from "./component/FunctionalComponent.tsx";
+import ArrowFunctionalComponent from "./component/ArrowFunctionalComponent.tsx";
+import ArrowFunctionalComponentWithProps from "./component/ArrowFunctionalComponentWithProps.tsx";
+import ArrowFunctionalComponentWithPropsType from "./component/ArrowFunctionalComponentWithPropsType.tsx";
 
 function App() {
-  const [count, setCount] = useState(0)
+
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+     <ClassComponent/>
+        <FunctionalComponent/>
+        <ArrowFunctionalComponent/>
+        <ArrowFunctionalComponentWithProps title="Is a Arrow Functional Componsent with Props"/>
+        <ArrowFunctionalComponentWithPropsType title="Is a Arrow Functional Componsent with Props"
+                                               description="This is a description"/>
     </>
   )
 }
