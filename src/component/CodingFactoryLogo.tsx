518875e9ce5453332fe3bff6f520d5d683ffd09a
Dim. Baltas <fenomeno979@gmail.com>
2025-07-31 09:21:23 +0300

    feat: add cf logo

---
+++ src/component/CodingFactoryLogo.tsx
@@ -0,0 +1,15 @@
+const CodingFactoryLogo = () =>  {
+    return (
+      <>
+
+       <img
+           className="my-4 h-16"
+           src="/logo.png"
+            alt="CF7"></img>
+
+      </>
+
+    )
+}
+
+export default CodingFactoryLogo;
